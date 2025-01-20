839ab33cc9e6549d3bde6c4767beb82a9398e209
alexandracampo <alexandracampo86@gmail.com>
2025-01-20 18:16:30 +0100

    add error message when no data

--- src/pages/WeatherInfoPage/WeatherInfo.jsx
+++ src/pages/WeatherInfoPage/WeatherInfo.jsx
@@ -4,6 +4,7 @@ import { useWeatherContext } from '../../context/WeatherContext';
 import WeatherIcon from './components/WeatherIcon.jsx';
 import Header from '../../components/Header.jsx';
 import { useNavigate } from "react-router-dom";
+import Error from '../WeatherInfoPage/components/Error.jsx';
 
 function WeatherInfo() {
   const { weatherData, selectedMunicip } = useWeatherContext();
@@ -14,26 +15,30 @@ function WeatherInfo() {
     setEstadoCielo(weatherData?.stateSky?.id);
   }, []);
 
+  const someError = !weatherData.temperatura_actual || !weatherData?.humedad
+
   // console.log(weatherData)
   return (
 
     <div className="weather-container">
       <Header title2={selectedMunicip?.name} showTitle2 />
 
-      <section className="weather-card">
-        <article className="card-item">{weatherData?.stateSky?.description}
-          <WeatherIcon estadoCielo={estadoCielo} />
-        </article>
+      {someError ? <Error /> :
 
-        <article className="card-item">Temperatura actual
-          <h1>{`${weatherData?.temperatura_actual}ºC`}</h1>
-        </article>
+        (<section className="weather-card">
+          <article className="card-item">{weatherData?.stateSky?.description}
+            <WeatherIcon estadoCielo={estadoCielo} />
+          </article>
 
-        <article className="card-item">Humedad
-          <h1>{`${weatherData?.humedad}%`}</h1>
-        </article>
+          <article className="card-item">Temperatura actual
+            <h1>{weatherData?.temperatura_actual ? `${weatherData.temperatura_actual}ºC` : '-'}</h1>
+          </article>
 
-      </section>
+          <article className="card-item">Humedad
+            <h1>{weatherData?.humedad ? `${weatherData?.humedad}%` : '-'}</h1>
+          </article>
+
+        </section>)}
 
       <footer className='footer-weatherinfo'>
         <button className='button-weatherinfo' aria-label="Volver a la página principal para hacer una nueva búsqueda" onClick={() => navigate("/")} >Nueva búsqueda</button>
---
+++ src/pages/WeatherInfoPage/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../styles/weather.css';
+
+const Error = () => {
+    return (
+        <div className='error'>
+            <p >Ha habido un error, inicia de nuevo la búsqueda.</p>
+        </div>
+    );
+};
+
+export default Error;
