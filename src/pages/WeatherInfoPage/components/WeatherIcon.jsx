04519444afedb44ad9f96b55a029cccf1aec7879
alexandracampo <alexandracampo86@gmail.com>
2025-01-20 20:52:31 +0100

    edit icons function

--- src/pages/WeatherInfoPage/components/WeatherIcon.jsx
+++ src/pages/WeatherInfoPage/components/WeatherIcon.jsx
@@ -11,30 +11,49 @@ import '../../../styles/weather.css';
 const WeatherIcon = ({ stateSky }) => {
 
     const icons = {
-        11: despejadoIcon,   // Despejado
-        12: despejadoIcon,   // Poco nuboso
-        13: cubiertoIcon,    // Nuboso
-        14: cubiertoIcon,    // Muy nuboso
-        15: cubiertoIcon,    // Cubierto
-        16: cubiertoIcon,    // Nubes altas
-        17: cubiertoIcon,    // Intervalos nubosos
-        18: lluviaIcon,      // Chubascos
-        19: tormentaIcon,    // Tormenta
-        20: nieblaIcon,      // Niebla
-        21: nieveIcon,       // Nieve   
-        22: lluviaIcon,      // Lluvia
-        23: lluviaIcon,      // Llovizna
-        24: lluviaIcon,      // Granizo
-        25: nieblaIcon,      // Calima
-        26: lluviaIcon,     // Cubierto con lluvia
-        44: lluviaIcon,     // Nuboso con lluvia escasa
-        46: lluviaIcon     // Cubierto con lluvia escasa
+        11: despejadoIcon, // Despejado (día y noche)
+        12: cubiertoIcon,  // Poco nuboso
+        13: cubiertoIcon,  // Nuboso
+        14: cubiertoIcon,  // Muy nuboso
+        15: cubiertoIcon,  // Cubierto
+        16: cubiertoIcon,  // Nubes altas
+        17: cubiertoIcon,  // Intervalos nubosos
+        23: lluviaIcon,    // Llovizna (día y noche)
+        24: lluviaIcon,    // Granizo
+        25: nieblaIcon,    // Calima
+        26: lluviaIcon,    // Cubierto con lluvia
+        33: nieveIcon,     // Nieve
+        34: nieveIcon,     // Nieve
+        35: nieveIcon,     // Nieve
+        36: nieveIcon,     // Nieve
+        43: lluviaIcon,    // Intervalos nubosos con lluvia escasa
+        44: lluviaIcon,    // Nuboso con lluvia escasa
+        45: lluviaIcon,    // Nuboso con lluvia
+        46: lluviaIcon,    // Cubierto con lluvia escasa
+        51: tormentaIcon,  // Tormenta
+        52: tormentaIcon,  // Tormenta
+        53: tormentaIcon,  // Tormenta
+        54: tormentaIcon,  // Tormenta
+        61: tormentaIcon,  // Tormenta
+        62: tormentaIcon,  // Tormenta
+        63: tormentaIcon,  // Tormenta
+        64: tormentaIcon,  // Tormenta
+        71: nieveIcon,     // Nieve
+        72: nieveIcon,     // Nieve
+        73: nieveIcon,     // Nieve
+        74: nieveIcon,     // Nieve
+        81: nieblaIcon,    // Niebla
+        82: nieblaIcon     // Niebla
     };
 
-    const imagen = icons[stateSky] || defaultIcon;
+    // Remover la letra 'n' al final del código si existe
+    const normalizedStateSky = stateSky?.replace('n', '');
+
+    // Buscar el ícono correspondiente o usar el ícono por defecto
+    const selectedIcon = icons[normalizedStateSky] || defaultIcon;
 
     return (
-        <img src={imagen} alt={imagen} className='icon' />
+        <img src={selectedIcon} alt={selectedIcon} className='icon' />
     );
 };
 
