7126045c6ea870320a9db0ff3c2cb4e6b398ae1c
alexandracampo <alexandracampo86@gmail.com>
2025-01-20 15:56:57 +0100

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/header.css';
+
+function Header({ title2, showTitle2 = true }) {
+
+    return (
+        <div className='header-container'>
+            <h2 className='header-text1'>WeatherInfo</h2>
+            {showTitle2 && <h2 className='header-text2'>{title2}</h2>}
+        </div>
+    );
+}
+
+export default Header;
